a552bea265210392728d181ba31db4ab25e40236
Szabolcs Tóth <tsz@purzelbaum.hu>
2025-10-04 21:49:39 +0200

    File headers were changed.

--- FileLift/FileLift/Views/DropzoneView.swift
+++ FileLift/FileLift/Views/DropzoneView.swift
@@ -2,15 +2,34 @@
 //  DropzoneView.swift
 //  FileLift
 //
-//  Created by Szabolcs Tóth on 04.10.2025.
-//  Copyright © 2025 Szabolcs Tóth. All rights reserved.
+//  Created by Szabolcs Tóth on 03.10.2025.
 //
+//  This file is part of FileLift and is licensed under the MIT License.
+//  Copyright © 2025 Szabolcs Tóth.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 
 import SwiftUI
 import UniformTypeIdentifiers
 
 struct DropzoneView: View {
-    @Environment(DataViewModel.self) private var vm
+  @Environment(DataViewModel.self) private var vm
   @State private var isDropActive = false
   @State private var dropzoneWidth: CGFloat = 340
   @State private var dropzoneHeight: CGFloat = 200
@@ -36,9 +55,9 @@ struct DropzoneView: View {
               print("Folders are not allowed: \(url.lastPathComponent)")
               return
             }
-              Task {
-                  try await vm.putObject(filePath: url.path)
-              }
+            Task {
+              try await vm.putObject(filePath: url.path)
+            }
             print("Dropped file name: \(url.lastPathComponent)")
           }
         }
@@ -55,5 +74,5 @@ struct DropzoneView: View {
 // MARK: - Preview
 #Preview {
   DropzoneView()
-        .environment(DataViewModel.preview)
+    .environment(DataViewModel.preview)
 }
