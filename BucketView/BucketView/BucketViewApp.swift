7520979fc452bfbf8dfec421a2be1acda23c3061
Szabolcs Tóth <tsz@purzelbaum.hu>
2025-10-05 16:42:13 +0200

    Initial commit with developer icon and oci-swift-sdk package.

---
+++ BucketView/BucketView/BucketViewApp.swift
@@ -0,0 +1,18 @@
+//
+//  BucketViewApp.swift
+//  BucketView
+//
+//  Created by Szabolcs Tóth on 05.10.2025.
+//  Copyright © 2025 Szabolcs Tóth. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct BucketViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketView/BucketView/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  BucketView
+//
+//  Created by Szabolcs Tóth on 05.10.2025.
+//  Copyright © 2025 Szabolcs Tóth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
